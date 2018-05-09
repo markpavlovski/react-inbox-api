63351eb64cd3adb29a6ae18fae05867652d327ce
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-09 14:11:42 -0700

    added messages component and put the message components inside of that

---
+++ src/components/Messages.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import Message from './Message'
+
+
+class Messages extends Component {
+  render(){
+    return (
+      <Message />
+    )
+  }
+}
+
+
+export default Messages

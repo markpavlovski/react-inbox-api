723e1f45e20f19dc9f774d2eb977ce773728ef23
Mark Pavlovski <markpavlovski@gmail.com>
2018-05-09 21:10:51 -0700

    hooked up messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,26 +1,33 @@
 import React from 'react'
 
-const Message = props => {
-    return (
-      <div className = {`row message` + (props.read ? 'row message read' : 'row message unread')}>
-        <div className="col-xs-1">
-          <div className="row">
-            <div className="col-xs-2">
-              <input type="checkbox" />
-            </div>
-            <div className="col-xs-2">
-              <i className={'star fa fa-star' + (props.starred ? '' : '-o')}></i>
-            </div>
-          </div>
+const Message = props => (
+  <div className = {[
+    'row',
+    'message',
+     props.read ? 'read' : 'unread',
+     props.selected ? 'selected' : ''
+   ].join(' ')}>
+    <div className="col-xs-1">
+      <div className="row">
+        <div className="col-xs-2">
+          <input type="checkbox" />
         </div>
-        <div className="col-xs-11">
-          <a href="#">
-            {props.subject}
-          </a>
+        <div className="col-xs-2">
+          <i className={[
+            'star',
+            'fa',
+            props.starred ? 'fa-star' : 'fa-star-o'
+          ].join(' ')}/>
         </div>
       </div>
-    )
-  }
+    </div>
+    <div className="col-xs-11">
+      <a href="#">
+        {props.subject}
+      </a>
+    </div>
+  </div>
+)
 
 
 export default Message
